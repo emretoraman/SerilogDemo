20d6903febba5cbb1a6af9581569b6445246ce1f
Emre Toraman <emretoraman@gmail.com>
2021-07-08 19:57:51 +0300

    Logs

--- SerilogDemo/Pages/Index.cshtml.cs
+++ SerilogDemo/Pages/Index.cshtml.cs
@@ -1,16 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SerilogDemo.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly ILogger _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -19,7 +15,20 @@ namespace SerilogDemo.Pages
 
         public void OnGet()
         {
+            _logger.LogInformation("You requested the Index page");
 
+            try
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    if (i == 56) throw new Exception("This is our demo exception");
+                    _logger.LogInformation("The value of i is {LoopCountValue}", i);
+                }
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, "We caught this exception in the Index Get call.");
+            }
         }
     }
 }
